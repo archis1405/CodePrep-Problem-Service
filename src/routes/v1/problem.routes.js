57d1bf20e7578da9f0b6dbb88466511f2823f724
archis1405 <archismandas1405@gmail.com>
2025-07-06 17:16:38 +0530

    Feat : Added Routing struture for the versioned API problems

---
+++ src/routes/v1/problem.routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const ProblemController = require('../../controllers/problem_controller');
+
+const problemRouter = express.Router();
+problemRouter.get('/ping', ProblemController.pingProblemController);
+
+problemRouter.get('/:id', ProblemController.getProblem);
+
+problemRouter.get('/', ProblemController.getAllProblems);
+
+problemRouter.post('/', ProblemController.addProblem);
+
+problemRouter.put('/:id', ProblemController.updateProblem);
+
+problemRouter.delete('/:id', ProblemController.deleteProblem);
+
+
+
+
+module.exports = problemRouter;
