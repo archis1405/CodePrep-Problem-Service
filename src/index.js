02b8350627e34df47cbddfdd4b182373c91d9f24
archis1405 <archismandas1405@gmail.com>
2025-07-07 23:12:23 +0530

    Feat : Added the DB Config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const {PORT} = require('./config/server_config');
 const apiRouter = require('./routes');
 
 const errorHandler = require('./utils/errorHandler');
+const connectToDB = require('./config/db.config');
+
 
 
 const app =  express();
@@ -23,8 +25,11 @@ app.get('/ping' , (req ,res) => {
 // last middleware if any errors occurs 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
     console.log(`Server is running on port ${PORT}`);
+    
+    await connectToDB();
+    console.log("Sucessfully Connected to DB");
 
 });
 
